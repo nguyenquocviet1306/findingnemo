8718a28ac229f099b7312e8957b1b8c1a5a99040
Duc Nhan <ducnhanyb@gmail.com>
2018-10-09 07:59:47 +0000

    Add template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_filter :authenticate_user!
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -9,12 +10,12 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit :user_nick_name, :user_email, :user_age, :user_gender,
-        :user_birthday, :user_picture, :user_password
+      user_params.permit :nick_name, :email, :gender, :birthday, :user_picture,
+        :password
     end
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
-      user_params.permit :user_nick_name, :user_email, :user_age, :user_gender,
-        :user_birthday, :user_picture, :user_password
+      user_params.permit :nick_name, :email, :gender, :birthday, :user_picture,
+        :password
     end
   end
 end
