389433d2788f7c4edc9b990b5dee83d8388af464
nguyenquocviet1306 <nguyenquocviet1306@gmail.com>
2018-10-23 22:36:36 +0700

    fix bug

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -61,8 +61,6 @@ class HotelsController < ApplicationController
       format.json { head :no_content }
     end
   end
-  def search
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
