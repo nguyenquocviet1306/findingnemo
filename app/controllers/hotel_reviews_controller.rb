403446d9f536c2d485566f39aca2bc11e147fafc
Phan Thanh Thư <thanhthuk59@gmail.com>
2018-11-06 17:21:04 +0000

    save comment

--- app/controllers/hotel_reviews_controller.rb
+++ app/controllers/hotel_reviews_controller.rb
@@ -29,6 +29,10 @@ class HotelReviewsController < ApplicationController
   # POST /hotel_reviews.json
   def create
     @hotel_review = HotelReview.new(hotel_review_params)
+    @hotel_review.space_rating = params[:space_rating]
+    @hotel_review.service_rating = params[:service_rating]
+    @hotel_review.security_rating = params[:security_rating]
+    @hotel_review.clean_rating = params[:clean_rating]
 
     respond_to do |format|
       if @hotel_review.save
