65dd10b47dcee14905a9efe2b4f8b70419f9aa7e
trần minh quang <mquang0603@gmail.com>
2018-11-13 03:56:35 +0000

    average rating of comment

--- app/models/hotel_review.rb
+++ app/models/hotel_review.rb
@@ -1,4 +1,9 @@
 class HotelReview < ActiveRecord::Base
     belongs_to :user
     belongs_to :hotel
+    
+    def get_avarage
+        
+        ((space_rating+ service_rating+ security_rating+ clean_rating).to_f/4)
+    end
 end
